ff2907c29fc7af22a640b6c6659caab667970494
usman-369 <m.usman.8460@gmail.com>
2025-01-30 15:17:43 +0500

    Updated the comment for tabulate

--- assignment.py
+++ assignment.py
@@ -266,8 +266,9 @@ def list_all_students():
 
     print("\nListing all students:\n")
 
-    # You can use [A] part if you do not have the "tabulate" library installed
-    # but "tabulate" is recomended.
+    # You can use [A] part if you do not have the "tabulate" library installed.
+    # But it's gonna be a reeeeeally long list if you have a large amount
+    # of data, thats why "tabulate" is recomended. It's much better. ;)
 
     # [A]: {
     # for line in data:
